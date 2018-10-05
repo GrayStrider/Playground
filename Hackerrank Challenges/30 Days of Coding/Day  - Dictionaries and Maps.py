fde030006a7a0a6358a2852ebb84eee9be28283a
GrayStrider <GrayStrider@users.noreply.github.com>
2018-10-05 18:46:01 +1100

    Day 8

---
+++ Hackerrank Challenges/30 Days of Coding/Day  - Dictionaries and Maps.py
@@ -0,0 +1,20 @@
+"""
+
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+
+
+"""
+import sys
+
+# fill the dictionary
+for i in range(input()):
+
+# compare with dictionary and print the results
+for line in sys.stdin:
+
