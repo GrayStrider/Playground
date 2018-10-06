ff33f1f590d41876f69a94963deca301e807f1c9
GrayStrider <GrayStrider@users.noreply.github.com>
2018-10-06 15:39:37 +1100

    Day 8 - Done

---
+++ Hackerrank Challenges/30 Days of Coding/Day 8 - Dictionaries and Maps.py
@@ -0,0 +1,30 @@
+"""
+
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+tom
+jerry
+
+Reading space-separated input
+stripping \n character from stdin
+
+"""
+
+import sys
+
+book = {}
+# fill the dictionary
+for i in range(int(input())):
+    name, number = input().split()
+    book[name] = number
+
+# compare with dictionary and print the results
+for line in sys.stdin:
+    line_ = line.rstrip("\n")
+    if line_ in book:
+        print("%s=%s" % (line_, book[line_]))
+    else:
+        print("Not found")
