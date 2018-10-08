5dfeb9c403b340fd1ee8e145a9d00f0e3da1f50d
GrayStrider <GrayStrider@users.noreply.github.com>
2018-10-08 18:17:18 +1100

    Day 12 - Done

---
+++ Hackerrank Challenges/30 Days of Coding/Day 12 - Inheritance.py
@@ -0,0 +1,55 @@
+"""
+
+Heraldo Memelli 8135627 (First, Last, id)
+2 (number of scores)
+100 80 (scores)
+
+Really bad. Too long, to grindy. Too bad.
+
+"""
+
+
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+
+class Student(Person):
+    def __init__(self, firstName, lastName, id, scores):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = id
+        self.scores = scores
+
+    def calculate(self):
+        if int(sum(self.scores) / len(scores)) in range(90, 101):
+            return "O"
+        elif int(sum(self.scores) / len(scores)) in range(80, 91):
+            return "E"
+        elif int(sum(self.scores) / len(scores)) in range(70, 81):
+            return "A"
+        elif int(sum(self.scores) / len(scores)) in range(55, 71):
+            return "P"
+        elif int(sum(self.scores) / len(scores)) in range(40, 56):
+            return "D"
+        elif int(sum(self.scores) / len(scores)) < 40:
+            return "T"
+        else:
+            return 0
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input())  # not needed for Python
+scores = list(map(int, input().split()))
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
