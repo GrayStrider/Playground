d6862b42f994f17a804b23a244dec5cafc902aef
GrayStrider <GrayStrider@users.noreply.github.com>
2018-10-09 13:34:40 +1100

    Formatting

--- --- Index.py
+++ --- Index.py
@@ -17,5 +17,19 @@ class Person:
         self.name = name
         self.age = age
         self.species = "Homo Sapiens"
-
+        
+        
 # REV - s instead of self
+
+
+# REV - multiline input, sort of. Not the best example I think
+a = []
+while input() != "\n":
+    a.append(input())
+print(a)
+
+
+# REV - "range" inclusiveness
+# starts from 1, ends at 4.
+for i in range(1,5):
+    print(i)
--- Hackerrank Challenges/30 Days of Coding/range inclusive.py
+++
@@ -1,3 +0,0 @@
-# so: starts from 1, ends at 4.
-for i in range(1,5):
-    print(i)
